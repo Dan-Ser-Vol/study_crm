8a4f6db5fd55446029d54a8bbce552803bc5dce5
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2024-01-12 01:05:34 +0200

    added a few lines of code

--- client/src/app/modules/admin/pages/admin-page/admin-page.component.ts
+++ client/src/app/modules/admin/pages/admin-page/admin-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OrdersStatisticComponent } from '../../components/orders-statistic/orders-statistic.component';
+import {ManagerItemComponent} from "../../components/manager-item/manager-item.component";
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [OrdersStatisticComponent],
+  imports: [OrdersStatisticComponent, ManagerItemComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
 })
